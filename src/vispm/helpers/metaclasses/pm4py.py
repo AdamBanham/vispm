a964d3a058e75b7e1597e8a9b83a38aefbe6c84d
Adam Peter Banham <n7176546@qut.edu.au>
2022-06-14 21:36:40 +1000

    added some dummy metaclasses for typing.

---
+++ src/vispm/helpers/metaclasses/pm4py.py
@@ -0,0 +1,32 @@
+
+from collections.abc import Mapping, Sequence
+
+class Event(Mapping):
+    """
+    Dummy placeholder for pm4py.objects.log.obj.Event\n
+
+    source: https://github.com/pm4py/pm4py-core/blob/release/pm4py/objects/log/obj.py#L55 
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+class Trace(Sequence):
+    """
+    Dummy placeholder for pm4py.objects.log.obj.Trace\n
+
+    source: https://github.com/pm4py/pm4py-core/blob/release/pm4py/objects/log/obj.py#L211
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+class EventLog(Mapping,Sequence):
+    """
+    Dummy placeholder for pm4py.objects.log.obj.EventLog\n
+
+    source: https://github.com/pm4py/pm4py-core/blob/release/pm4py/objects/log/obj.py#L211
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
