01b2660f0ec8bfd091e4f686af863df86db78e92
Adam Peter Banham <n7176546@qut.edu.au>
2022-06-19 00:03:03 +1000

    added some suggested colormaps

---
+++ src/vispm/helpers/colours/colourmaps.py
@@ -0,0 +1,16 @@
+from matplotlib.cm import get_cmap
+from matplotlib.colors import ListedColormap
+
+import numpy as np
+
+CATEGORICAL = get_cmap("Accent")
+HIGH_CONTRAST_COOL = get_cmap("viridis", 25)
+HIGH_CONTRAST_WARM = get_cmap("plasma", 25)
+COOL_WINTER = get_cmap("YlGnBu")
+
+earth_top = get_cmap('Greens', 25)
+earth_bottom = get_cmap('copper', 100)
+
+newcolors = np.vstack((earth_top(np.linspace(0.75, 0.25, 6)),
+                       earth_bottom(np.linspace(1, .25, 12))))
+EARTH = ListedColormap(newcolors, name='EARTH')
