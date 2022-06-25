3e7fb0e0a305ad07653307df5083e1ac2b57246e
Adam Peter Banham <n7176546@qut.edu.au>
2022-06-25 15:42:47 +1000

    made extension available from vispm

--- src/vispm/__init__.py
+++ src/vispm/__init__.py
@@ -2,3 +2,8 @@
 # dotted chart presentors
 ## static presentors
 from .static.dotted import StaticDottedChartPresentor
+
+
+# Extensions
+## dotted chart extensions
+from .extensions.dotted.colour_histogram import DottedColourHistogramExtension
