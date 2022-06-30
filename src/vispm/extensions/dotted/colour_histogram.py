78b000c13871191fa2ed3264ac8cede4365fe10e
Adam Peter Banham <n7176546@qut.edu.au>
2022-06-30 19:43:09 +1000

    stabilised presentation of histograms

--- src/vispm/extensions/dotted/colour_histogram.py
+++ src/vispm/extensions/dotted/colour_histogram.py
@@ -3,6 +3,7 @@ from matplotlib.axis import Axis
 from .._base import ChartExtension
 from ...helpers.metaclasses.vispm import Presentor
 from ...static.dotted import StaticDottedChartPresentor
+from ...helpers.imputers.colour_imputers import ColourImputer
 
 from typing import Any, Tuple, List
 from enum import Enum, auto
@@ -30,8 +31,8 @@ class DottedColourHistogramExtension(ChartExtension):
     direction:`ChartExtension.Direction`\n
     [Optional] Sets what direction to build axes in for extension \n
     \n
-    plot_axes:`DottedColourHistogramExtension.PlotAxes=PlotAxes.X"\n
-    [Optional] Sets what axes to plot. X plots the colour of the events 
+    bin_axes:`DottedColourHistogramExtension.PlotAxes=PlotAxes.X"\n
+    [Optional] Sets what axes to use to generate bins. X will use the relative timestamp from start of event log, while Y will use the trace number. 
     \n
     debug:`bool=True`\n
     [Optional] Sets whether debug messages are printed.\n
@@ -46,12 +47,12 @@ class DottedColourHistogramExtension(ChartExtension):
 
     def __init__(self, 
         direction:ChartExtension.Direction=ChartExtension.Direction.SOUTH, 
-        plot_axes:PlotAxes=PlotAxes.Y,
+        bin_axes:PlotAxes=PlotAxes.Y,
         debug: bool = True) -> None:
         super().__init__(debug)
         # setup 
         self._direction = direction
-        self._plot_axes = plot_axes
+        self._bin_axes = bin_axes
         self._axes = None
         self._size = (1.0,1.0)
 
@@ -80,15 +81,14 @@ class DottedColourHistogramExtension(ChartExtension):
         else: 
             return ("yr", ( 60 * 60 * 24 * 365))
 
-    def draw(self, x_data:List[float], y_data:List[float], colors:List[Tuple[float,float,float,float]], *args, **kwags) -> Axes:
+    def draw(self, x_data:List[float], y_data:List[float], colors:List[Tuple[float,float,float,float]], colour_imputer:ColourImputer, *args, **kwags) -> Axes:
         # set plot axis 
         self._debug("ploting histogram...")
-        plot_axis = x_data if self._plot_axes == self.PlotAxes.X else y_data
-        # bin_space = np.linspace(min(plot_axis), max(plot_axis), 100)
-        # bins = [ () for start,end in zip(bin_space[:-1],bin_space[1:])]
+        plot_axis = x_data if self._bin_axes == self.PlotAxes.X else y_data
+
         x_rects = [] 
-        uni_colors = set(colors)
-        for color in  uni_colors:
+        seen_colors = colour_imputer.get_seen_order()
+        for color in  seen_colors:
             x_subset = [ x for x,c in zip(plot_axis, colors) if c.__eq__(color)]
             x_rects.append(x_subset)
 
@@ -98,49 +98,75 @@ class DottedColourHistogramExtension(ChartExtension):
             orientation = 'vertical'
 
         # plot
-        self._axes.hist(x_rects,stacked=True, histtype='barstacked', bins=100, color=uni_colors, orientation=orientation)
+        n,bins,_ = self._axes.hist(x_rects, histtype='barstacked', bins=100, color=seen_colors, orientation=orientation)
+
+        tops = [ 
+            max([ stack[i] for stack in n ])
+            for i 
+            in range(len(n[0]))  
+        ]
+        count_max = np.max(tops)
+        count_mid = np.floor(count_max/2.0)
+
 
         # add labels 
-        if self._plot_axes == self.PlotAxes.Y:
+        if self._bin_axes == self.PlotAxes.Y:
             if orientation == 'horizontal':
                 self._axes.set_ylabel("trace identifier")
                 self._axes.set_xlabel("No. events")
+                self._axes.set_xticks([0, count_mid, count_max])
+                self._axes.set_xlim(0, count_max)
             else:
                 self._axes.set_xlabel("trace identifier")
                 self._axes.set_ylabel("No. events")
+                self._axes.set_yticks([0, count_mid, count_max])
+                self._axes.set_ylim(0, count_max)
         else:
             if orientation == 'horizontal':
                 self._axes.set_ylabel("time of event")
                 self._axes.set_xlabel("No. events")
+                self._axes.set_xticks([0, count_mid, count_max])
+                self._axes.set_xlim(0, count_max)
             else:
                 self._axes.set_xlabel("time of event")
                 self._axes.set_ylabel("No. events")
+                self._axes.set_yticks([0, count_mid, count_max])
+                self._axes.set_ylim(0, count_max)
         
         # adjust xticks for timestamp
-        if  self._plot_axes == self.PlotAxes.X:
-
+        if  self._bin_axes == self.PlotAxes.X:
             if orientation == 'horizontal':
                 min_x = min(plot_axis)
-                suffix, scale = self._find_scale(max(plot_axis) - min_x)
+                max_x = max(plot_axis)
+                portion = (max_x - min_x) / 8.0
+                suffix, scale = self._find_scale(max_x - min_x)
+                ticks = [min_x] + [min_x + (i*portion) for i in range(8)] + [max_x]
+                self._axes.set_yticks(ticks)
                 self._axes.set_yticklabels(
                     [ 
                         f"{(tick - min_x) / scale:.1f}{suffix}"
                         for tick 
-                        in self._axes.get_yticks()
+                        in ticks
                     ],
                     rotation=-45
-                )    
+                )
+                self._axes.set_ylim([min_x,max_x])
             else :
                 min_x = min(plot_axis)
-                suffix, scale = self._find_scale(max(plot_axis) - min_x)
+                max_x = max(plot_axis)
+                portion = (max_x - min_x) / 8.0
+                suffix, scale = self._find_scale(max_x - min_x)
+                ticks = [min_x] + [(min_x + (i*portion)) for i in range(8)] + [max_x]
+                self._axes.set_xticks(ticks)
                 self._axes.set_xticklabels(
                     [ 
                         f"{(tick - min_x) / scale:.1f}{suffix}"
                         for tick 
-                        in self._axes.get_xticks()
+                        in ticks
                     ],
                     rotation=-45
                 )    
+                self._axes.set_xlim([min_x,max_x])
 
         # clean up axes
         
