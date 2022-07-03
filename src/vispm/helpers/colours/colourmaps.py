be176a06b5f8c90b64b86a8b9e69b74985624e0f
Adam Peter Banham <n7176546@qut.edu.au>
2022-07-03 21:19:08 +1000

    Update colourmaps.py

--- src/vispm/helpers/colours/colourmaps.py
+++ src/vispm/helpers/colours/colourmaps.py
@@ -4,11 +4,11 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 
 CATEGORICAL = get_cmap("Accent")
-HIGH_CONTRAST_COOL = get_cmap("viridis", 25)
-HIGH_CONTRAST_WARM = get_cmap("plasma", 25)
+HIGH_CONTRAST_COOL = get_cmap("viridis", 26)
+HIGH_CONTRAST_WARM = get_cmap("plasma", 26)
 COOL_WINTER = get_cmap("YlGnBu")
 
-earth_top = get_cmap('Greens', 25)
+earth_top = get_cmap('Greens', 26)
 earth_bottom = get_cmap('copper', 100)
 
 newcolors = np.vstack((earth_top(np.linspace(0.75, 0.25, 6)),
