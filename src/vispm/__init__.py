cb972732cfd38f317f244cadbe64cb85d5897577
Adam Peter Banham <n7176546@qut.edu.au>
2022-07-03 21:17:04 +1000

    added new extension to top-level api

--- src/vispm/__init__.py
+++ src/vispm/__init__.py
@@ -7,3 +7,4 @@ from .static.dotted import StaticDottedChartPresentor
 # Extensions
 ## dotted chart extensions
 from .extensions.dotted.colour_histogram import DottedColourHistogramExtension
+from .extensions.dotted.event_histogram import DottedEventHistogramExtension
